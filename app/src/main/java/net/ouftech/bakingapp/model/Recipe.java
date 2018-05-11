9a9b593bfa8a91fcdd466ee90099e4fbbbd86865
Antoine Purnelle <antoine.purnelle@ouftech.net>
2018-05-11 21:06:20 +0200

    Added model classes

---
+++ app/src/main/java/net/ouftech/bakingapp/model/Recipe.java
@@ -0,0 +1,99 @@
+/*
+ * Copyright 2018 Antoine PURNELLE
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.ouftech.bakingapp.model;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Recipe implements Parcelable {
+    public static final String ID_KEY = "id";
+    public static final String NAME_KEY = "name";
+    public static final String INGREDIENTS_KEY = "ingredients";
+    public static final String STEPS_KEY = "steps";
+    public static final String SERVINGS_KEY = "servings";
+
+    @SerializedName(ID_KEY)
+    public int id;
+    @SerializedName(NAME_KEY)
+    public String name;
+    @SerializedName(INGREDIENTS_KEY)
+    public List<Ingredient> ingredients;
+    @SerializedName(STEPS_KEY)
+    public List<Step> steps;
+    @SerializedName(SERVINGS_KEY)
+    public int servings;
+
+    protected Recipe(Parcel in) {
+        id = in.readInt();
+        name = in.readString();
+        if (in.readByte() == 0x01) {
+            ingredients = new ArrayList<>();
+            in.readList(ingredients, Ingredient.class.getClassLoader());
+        } else {
+            ingredients = null;
+        }
+        if (in.readByte() == 0x01) {
+            steps = new ArrayList<>();
+            in.readList(steps, Step.class.getClassLoader());
+        } else {
+            steps = null;
+        }
+        servings = in.readInt();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(id);
+        dest.writeString(name);
+        if (ingredients == null) {
+            dest.writeByte((byte) (0x00));
+        } else {
+            dest.writeByte((byte) (0x01));
+            dest.writeList(ingredients);
+        }
+        if (steps == null) {
+            dest.writeByte((byte) (0x00));
+        } else {
+            dest.writeByte((byte) (0x01));
+            dest.writeList(steps);
+        }
+        dest.writeInt(servings);
+    }
+
+    @SuppressWarnings("unused")
+    public static final Parcelable.Creator<Recipe> CREATOR = new Parcelable.Creator<Recipe>() {
+        @Override
+        public Recipe createFromParcel(Parcel in) {
+            return new Recipe(in);
+        }
+
+        @Override
+        public Recipe[] newArray(int size) {
+            return new Recipe[size];
+        }
+    };
+}
---
+++ app/src/main/java/net/ouftech/bakingapp/model/Step.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2018 Antoine PURNELLE
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.ouftech.bakingapp.model;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Step implements Parcelable {
+    public static final String ID_KEY = "id";
+    public static final String SHORT_DESCRIPTION_KEY = "shortDescription";
+    public static final String DESCRIPTION_KEY = "description";
+    public static final String VIDEO_URL_KEY = "videoURL";
+
+    @SerializedName(ID_KEY)
+    public int id;
+    @SerializedName(SHORT_DESCRIPTION_KEY)
+    public String shortDescription;
+    @SerializedName(DESCRIPTION_KEY)
+    public String description;
+    @SerializedName(VIDEO_URL_KEY)
+    public String videoUrl;
+
+    protected Step(Parcel in) {
+        id = in.readInt();
+        shortDescription = in.readString();
+        description = in.readString();
+        videoUrl = in.readString();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(id);
+        dest.writeString(shortDescription);
+        dest.writeString(description);
+        dest.writeString(videoUrl);
+    }
+
+    @SuppressWarnings("unused")
+    public static final Parcelable.Creator<Step> CREATOR = new Parcelable.Creator<Step>() {
+        @Override
+        public Step createFromParcel(Parcel in) {
+            return new Step(in);
+        }
+
+        @Override
+        public Step[] newArray(int size) {
+            return new Step[size];
+        }
+    };
+}
